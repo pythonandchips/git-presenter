98b905c3097bb90c0b6a58b0bebb06557f3d3a93
Colin Gemmell <pythonandchips@gmail.com>
2012-03-06 22:03:25 +0000

    added basic forward and back though presentation

--- lib/git_presenter.rb
+++ lib/git_presenter.rb
@@ -1,6 +1,11 @@
 require "grit"
 
-module GitPresenter
+class GitPresenter
+  def initialize(commits)
+    @commits = commits
+    @current_commit = commits.first
+  end
+
   def self.initialise_presentation dir
     File.open(dir + "/.presentation", "w") do |file|
       repo = Grit::Repo.init(".")
@@ -11,10 +16,44 @@ module GitPresenter
   end
 
   def self.start_presentation dir
+    presenter = nil
     File.open(dir + "/.presentation", "r") do |file|
       commits = file.lines.map{|line| line.strip}
-      first_commit_id = commits.first
-      `git checkout #{first_commit_id}`
+      presenter = GitPresenter.new(commits)
+      presenter.checkout_current
+      presenter.run
     end
   end
+
+  def run
+    while(continue)
+      puts "command>>"
+      command = gets
+      process_command(command)
+      continue = false
+    end
+  end
+
+  def process_command(command)
+    if command == "n"
+      @presenter.next
+    end
+  end
+
+  def next
+    position = commits.index(commit_id)
+    @current_commit = commits[position + 1]
+    checkout_current
+  end
+
+  def previous
+    position = commits.index(commit_id)
+    @current_commit = commits[position - 1]
+    checkout_current
+  end
+
+  def checkout_current
+    `git checkout #{current_commit}`
+  end
+
 end
--- spec/integration/git_presenter_spec.rb
+++ spec/integration/git_presenter_spec.rb
@@ -28,13 +28,10 @@ describe "using git presenter" do
       @commits = initialise_test_repo(presentation_dir)
       initialise_presentation(presentation_dir)
       start_presentation(presentation_dir)
+      next_commit(presentation_dir)
     end
 
-    subject do
-
-    end
-
-    it "should move the repository to the next commit" do
+    it "should move the head to the next commit" do
       Dir.chdir(presentation_dir) do
         repo = Grit::Repo.new(".")
         repo.commits.first.id.should eql @commits.second.id
@@ -42,22 +39,38 @@ describe "using git presenter" do
     end
   end
 
-  def should_be_on_commit dir, commit_id
-    Dir.chdir(dir) do
-      repo = Grit::Repo.new(".")
-      repo.commits.first.id.should eql @commits.second.id
+  context "when moving back through the presentation" do
+    before do
+      @commits = initialise_test_repo(presentation_dir)
+      initialise_presentation(presentation_dir)
+      previous_commit(presentation_dir)
+    end
+
+    it "should move the head back to the previous commit" do
     end
   end
 
   def initialise_presentation(dir)
     Dir.chdir(dir) do
-      GitPresenter.initialise_presentation(".")
+      @git_presentation = GitPresenter.initialise_presentation(".")
     end
   end
 
   def start_presentation(dir)
     Dir.chdir(dir) do
-      GitPresenter.start_presentation(".")
+      @git_presentation = GitPresenter.start_presentation(".")
+    end
+  end
+
+  def next_commit(dir)
+    Dir.chdir(dir) do
+      @git_presentation.next(".")
+    end
+  end
+
+  def previous_commit(dir)
+    Dir.chdir(dir) do
+      @git_presentation.previous(".")
     end
   end
 end
