58c3a44283d9c7f00767e249884cf1cfe6e206c2
Colin Gemmell <pythonandchips@gmail.com>
2012-03-19 23:52:12 +0000

    added command handling

--- lib/git_presenter.rb
+++ lib/git_presenter.rb
@@ -29,16 +29,55 @@ class GitPresenter
     presenter
   end
 
-  def execute(command)
-
+  def command_for(command)
+    return :commit if command =~ /^[0-9]+$/
+    {"n" => :next, "next" => :next,
+     "back" => :previous, "b" => :previous,
+     "start" => :start, "s" => :start,
+     "end" => :end, "e" => :end,
+     "list" => :list, "l" => :list,
+     "help" => :help, "h" => :help
+    }[command]
   end
+
+  def execute(user_command)
+    command = command_for(user_command)
+    return commit(user_command.to_i) if command == :commit
+    self.send(command)
+  end
+
+  def status_line
+    "#{position+1}/#{total_slides} >"
+  end
+
+  def position
+    commits.index(@current_commit)
+  end
+
+  def total_slides
+    @commits.length
+  end
+
 
   def start
     @current_commit = commits.first
     checkout_current
   end
 
-  def finish
+  def help
+<<-EOH
+Git Presenter Reference
+
+next/n: move to next slide
+back/b: move back a slide
+end/e:  move to end of presentation
+start/s: move to start of presentation
+list/l : list slides in presentation
+help/h: display this message
+EOH
+  end
+
+  def end
     @current_commit = commits.last
     checkout_current
   end
@@ -49,7 +88,6 @@ class GitPresenter
   end
 
   def next
-    position = commits.index(@current_commit)
     return if position.nil?
     @current_commit = commits[position + 1]
     checkout_current
--- spec/integration/moving_through_presentation_spec.rb
+++ spec/integration/moving_through_presentation_spec.rb
@@ -6,15 +6,15 @@ describe "while giving presentation" do
   context "when moving to the next slide" do
     it "should move to the next commit" do
       start_presentation do |commits, presenter|
-        presenter.next
+        presenter.execute("next")
         head_position.should eql commits[1].id
       end
     end
 
     it "should continue till the last commit" do
       start_presentation do |commits, presenter|
-        presenter.next
-        presenter.next
+        presenter.execute("next")
+        presenter.execute("next")
         head_position.should eql commits[2].id
       end
     end
@@ -23,10 +23,10 @@ describe "while giving presentation" do
   context "when the presentation reaches the end" do
     it "should stay on the last commit" do
       start_presentation do |commits, presenter|
-        presenter.next
-        presenter.next
-        presenter.next
-        presenter.next
+        presenter.execute("next")
+        presenter.execute("next")
+        presenter.execute("next")
+        presenter.execute("next")
         head_position.should eql commits[2].id
       end
     end
@@ -35,9 +35,9 @@ describe "while giving presentation" do
   context "when going back through the presentation" do
     it "should o go back to a previous commit" do
       start_presentation do |commits, presenter|
-        presenter.next
-        presenter.next
-        presenter.previous
+        presenter.execute("next")
+        presenter.execute("next")
+        presenter.execute("back")
         head_position.should eql commits[1].id
       end
     end
@@ -46,9 +46,9 @@ describe "while giving presentation" do
   context "when going back to the start of the presention" do
     it "should move to the first commit" do
       start_presentation do |commits, presenter|
-        presenter.next
-        presenter.next
-        presenter.start
+        presenter.execute("next")
+        presenter.execute("next")
+        presenter.execute("start")
         head_position.should eql commits[0].id
       end
     end
@@ -57,7 +57,7 @@ describe "while giving presentation" do
   context "when going to the end of the presentation" do
     it "should move the last commit" do
       start_presentation do |commits, presenter|
-        presenter.finish
+        presenter.execute("end")
         head_position.should eql commits.last.id
       end
     end
@@ -66,7 +66,7 @@ describe "while giving presentation" do
   context "when going to a specific slide" do
     it "should checkout the specific commit" do
       start_presentation do |commits, presenter|
-        presenter.commit(2)
+        presenter.execute("2")
         head_position.should eql commits[1].id
       end
     end
@@ -77,8 +77,27 @@ describe "while giving presentation" do
       start_presentation do |commits, presenter|
         commits[0] = "*#{commits[0]}"
         expected_output = commits.join("\n")
-        presentation = presenter.list
+        presentation = presenter.execute("list")
         presentation.should eql expected_output
+      end
+    end
+  end
+
+  context "when asking for help" do
+    it "should print a list of command and there usage" do
+      start_presentation do |commits, presenter|
+      help_text = <<-EOH
+Git Presenter Reference
+
+next/n: move to next slide
+back/b: move back a slide
+end/e:  move to end of presentation
+start/s: move to start of presentation
+list/l : list slides in presentation
+help/h: display this message
+EOH
+        message = presenter.execute("help")
+        message.should eql help_text
       end
     end
   end
